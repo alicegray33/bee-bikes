be9b5bf2a3b7a9bc12c698724979b2d857f763eb
ktn0817 <kashif.atcha@hotmail.co.uk>
2021-12-15 15:59:49 +0000

    end of chapter 14

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -14,7 +14,7 @@ describe DockingStation do
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
 
   it 'docks bike' do
     bike = Bike.new
@@ -30,6 +30,13 @@ describe DockingStation do
     
     it 'raise an error when no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock' do
+    it 'raises an error when full' do 
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
 end
