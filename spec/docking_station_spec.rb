873e6e4610ca66f9ccc1da1286ee3a8ce6fa0060
ktn0817 <kashif.atcha@hotmail.co.uk>
2021-12-15 15:10:59 +0000

    end of chapter 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,7 @@ describe DockingStation do
   end
 
   it 'releases working bikes' do
+    subject.dock(Bike.new)
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -19,5 +20,16 @@ describe DockingStation do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
-  
+
+  describe '#release bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end 
+    
+    it 'raise an error when no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
 end
