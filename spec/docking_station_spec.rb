1e62e5260e0aab7bdc5a04be60736e073e2d6ece
Alice Gray <alicegray33@gmail.com>
2021-12-15 14:43:34 +0000

    end of chapter 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,13 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks bike' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+  
 end
